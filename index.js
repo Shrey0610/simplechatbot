56ebbc21e5cee64e1fef43f1a215961f4c6088bb
Shrey0610 <shreyshah241@gmail.com>
2024-02-10 00:02:52 +0530

    updates

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
+// import OpenAI from 'openai';
+
+// const openai = new OpenAI({
+//   apiKey: 'YOUR_API_KEY', // This is the default and can be omitted
+// });
+
+
+// async function main() {
+
+//     const completions = await openai.chat.completions.create({
+//       messages: [{ role: 'user', content: 'What is the color of the sky' }],
+//       model: 'gpt-3.5-turbo',
+//     });
+//     console.log(completions.choices);
+// }
+
+// main();
+
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
   apiKey: 'YOUR_API_KEY', // This is the default and can be omitted
 });
 
+const messages= [];
 
-async function main() {
-
+async function main(input) {
+  messages.push({role: "user", content: input});
     const completions = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: 'What is the color of the sky' }],
+      messages: messages,
+      
       model: 'gpt-3.5-turbo',
     });
-    console.log(completions.choices);
+    console.log(`${completions.choices[0].message.content}`);
 }
 
-main();
+
+  rl.on('line', (input) => {
+    main(input);
+    if(input === ''){
+      rl.close();
+    }
+  });
